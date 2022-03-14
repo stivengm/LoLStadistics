02772badc38feada219e06f7051a21dba2a8fd14
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-14 11:37:18 -0500

    Add theme dark app.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: const Color(0xFF141414),
+    colorScheme: const ColorScheme.dark(),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black
+    )
+  );
+
+}

25796784708e570e555a252418d5c82be19d02de
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-14 15:02:17 -0500

    Add champions page.

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:lol_stadistics/gui/dashboard/dashboard_view.dart';
+import 'package:lol_stadistics/gui/views/champions_view/champions_view.dart';
+import 'package:lol_stadistics/gui/views/dashboard_view/dashboard_view.dart';
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
-  'dashboard':         ( _ ) => const DashboardView(),
+  'dashboard':  ( _ ) => const DashboardView(),
+  'champions':  ( _ ) => const ChampionsView(),
 };
---
+++ lib/gui/views/champions_view/champions_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lol_stadistics/core/blocs/champions/champions_bloc.dart';
+
+class ChampionsView extends StatelessWidget {
+  const ChampionsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Champions"),
+        centerTitle: false,
+      ),
+      body: BlocBuilder<ChampionsBloc, ChampionsState>(
+        builder: (context, state) {
+          return state.champions != null ? 
+            ListView.builder(
+              itemCount: state.champions!.data!.length,
+              itemBuilder: ( context, index ) {
+                String champion = state.champions!.data!.keys.elementAt(index);
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.network('http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${state.champions!.data![champion]!.id!}.png'),
+                          ),
+                          Text(state.champions!.data![champion]!.name!, style: Theme.of(context).textTheme.headline6)
+                        ],
+                      ),
+                      const Divider()
+                    ],
+                  ),
+                );
+              }
+            ) : 
+            const Center(
+              child: CircularProgressIndicator()
+            );
+        },
+      ),
+    );
+  }
+}

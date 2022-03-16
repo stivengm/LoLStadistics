45c36a675ba5f1fac6f4b24418969abed0bdfd19
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-16 17:46:48 -0500

    Fix home.

--- lib/gui/views/dashboard_view/dashboard_view.dart
+++ lib/gui/views/dashboard_view/dashboard_view.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:lol_stadistics/gui/views/store_app.dart';
+import 'package:lol_stadistics/gui/widgets/flush_bar_widget.dart';
+import 'package:lol_stadistics/gui/widgets/primary_button_widget.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
 
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final _store = StoreAppView();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Champions"),
         centerTitle: false,
         leading: IconButton(
           icon: const Icon(
@@ -17,17 +25,58 @@ class DashboardView extends StatelessWidget {
           onPressed: () => ZoomDrawer.of(context)!.toggle(),
         ),
       ),
-      body: Column(
-        children: [
-          const Text("Dashboard"),
-          IconButton(
-            icon: const Icon(
-              Icons.add
+      body: _body()
+    );
+  }
+
+  Widget _body() {
+    Size _media = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        color: Colors.transparent,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: _media.width * .6,
+                  child: TextFormField(
+                    onChanged: (value) {
+                      _store.nameInvocador = value;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Nombre de invicador',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white
+                        )
+                      )
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 5.0),
+                SizedBox(
+                  width: _media.width * .25,
+                  child: DropdownButtonFormField(
+                    items: _store.itemsDropDownRegion(),
+                    decoration: InputDecoration(
+                        hintText: 'Región',
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
+                    onChanged: (value) {}
+                  ),
+                ),
+              ],
             ),
-            onPressed: () => Navigator.pushNamed(context, 'profileLol'),
-          )
-        ],
-      )
+            const SizedBox(height: 20.0),
+            PrimaryButton(
+              text: 'Buscar',
+              onPressed: () => _store.nameInvocador != '' ? Navigator.pushNamed(context, 'profileLol') : const FlushBarWidget(message: "Por favor escriba el nombre del invocador.",).showNotification(context),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/gui/views/store_app.dart
+++ lib/gui/views/store_app.dart
@@ -1,6 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lol_stadistics/data/models/region_model.dart';
 
 class StoreAppView {
 
+  String nameInvocador = "";
+
+  List<RegionModel> region = [
+    RegionModel(text: 'LAN', value: 'la1'),
+    RegionModel(text: 'LAS', value: 'la2'),
+  ];
+
+  List<DropdownMenuItem<String>> itemsDropDownRegion() => region
+  .map((regionItem) => DropdownMenuItem(
+    child: Text(regionItem.text),
+    value: regionItem.value,
+  )).toList();
 
   static final StoreAppView _singleton = StoreAppView._();
   factory StoreAppView() => _singleton;
